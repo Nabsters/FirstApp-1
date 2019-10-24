2a04db5e5d3da353d29a7a54d389caa7cc4bded7
Hassanudin <siti.n.hassanudin@accenture.com>
2019-10-24 11:42:57 +0800

    Initial commit

---
+++ src/main/java/com/programwitherik/restservice/FirstApp/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.programwitherik.restservice.FirstApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/helloworld")
+    public String index()
+    {
+        return "Hello World";
+    }
+}
