7767e5c611e38c893d0eb1377a290777a5051598
Nabsters <56912267+Nabsters@users.noreply.github.com>
2019-10-25 19:09:57 +0800

    Add files via upload

--- src/main/java/com/programwitherik/restservice/FirstApp/HelloWorld.java
+++ src/main/java/com/programwitherik/restservice/FirstApp/HelloWorld.java
@@ -1,13 +1,21 @@
 package com.programwitherik.restservice.FirstApp;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/helloworld")
 @RestController
+
 public class HelloWorld {
-    @RequestMapping("/helloworld")
-    public String index()
+
+   @Value("${spring.message}")
+   private String message;
+
+   @GetMapping
+    public String hello()
     {
-        return "Hello World";
+        return message;
     }
 }
