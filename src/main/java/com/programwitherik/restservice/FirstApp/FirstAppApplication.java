42bbcd710f13c4ca37dd49c38fe2f0296245bbd4
Nabsters <56912267+Nabsters@users.noreply.github.com>
2019-10-30 14:37:33 +0800

    Add files via upload

--- src/main/java/com/programwitherik/restservice/FirstApp/FirstAppApplication.java
+++ src/main/java/com/programwitherik/restservice/FirstApp/FirstAppApplication.java
@@ -1,15 +1,13 @@
 package com.programwitherik.restservice.FirstApp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 
 public class FirstAppApplication{
-
-    static Logger logger = LoggerFactory.getLogger(FirstAppApplication.class);
 
     public static void main(String[] args)
     {
