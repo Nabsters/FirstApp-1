8fad465f0d1bf4520dfd66c9a971690b934ce284
siti.n.hassanudin <siti.n.hassanudin@accenture.com>
2019-10-30 15:18:58 +0800

    1.0

--- src/main/java/com/programwitherik/restservice/FirstApp/FirstAppApplication.java
+++ src/main/java/com/programwitherik/restservice/FirstApp/FirstAppApplication.java
@@ -1,13 +1,23 @@
 package com.programwitherik.restservice.FirstApp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FirstAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstAppApplication.class, args);
-	}
+@SpringBootApplication
+
+public class FirstAppApplication{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FirstAppApplication.class, args);
+    }
 
 }
+
+
+
+
+
+
